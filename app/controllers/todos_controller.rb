e6e6d0b4e51d99011db8ff0df4cba4165557ac91
kswarna2000 <kswarna2000@gmail.com>
2020-03-09 06:15:45 +0530

    small modification in the static message

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,6 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   def index
-    render plain: "Hello, this is /todos!"
+    render plain: "Hello, this is /todos and the time is #{DateTime.now.to_s(:short)}!"
   end
 end
