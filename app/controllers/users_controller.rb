7151b6899ad4df8f836144d373732f6b6a0fec95
kswarna2000 <kswarna2000@gmail.com>
2020-03-14 04:58:36 +0530

    Added 1) list users feature after creating users table using migrations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,23 @@ class UsersController < ApplicationController
   def index
     render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
   end
+
+  def show
+    id = params[:id]
+    user = User.find(id)
+    render plain: user.to_pleasant_string
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+    response_text = "Hey, your new user is created with is #{new_user.id} "
+    render plain: response_text
+  end
 end
