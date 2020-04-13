655646918616b13f2442409e6362fb8735b4ed6c
kswarna2000 <kswarna2000@gmail.com>
2020-04-13 23:05:39 +0530

    Added signin page and modified homepage

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   resources :todos
   resources :users
   post "users/login", to: "users#login"
+  get "/signin" => "sessions#new", as: :new_session
+  post "/signin" => "sessions#create", as: :session
 end
